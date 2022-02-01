a00721cb16c3147bb7e9d5ae5abd767437e64a19
Gilbert D. Cuerbo <gilbert.cuerbo@gmail.com>
2022-02-01 23:02:48 +0800

    update main nav

--- src/common/navs/mainNav.jsx
+++ src/common/navs/mainNav.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+
+import { useState, useEffect } from 'react'
+import Link from '@mui/material/Link'
+import Container from '@mui/material/Container'
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+import TextField from '@mui/material/TextField'
+import LoginIcon from '@mui/icons-material/Login'
+import Button from '@mui/material/Button'
+import CircularProgress from '@mui/material/CircularProgress';
 
 const MainNav = (props) => {
-    return
+
+    const logout = () => {
+
+    }
+
+    const redirect = () => {
+        
+    }
+
+    useEffect(() => {
+
+    }, [])
+
+    return (
+        <Grid container spacing={2}>
+            <Grid item xs={12}>
+                
+            </Grid>
+
+            <Grid item xs={12}>
+            </Grid>
+        </Grid>
+    )
 }
 
 export default MainNav
