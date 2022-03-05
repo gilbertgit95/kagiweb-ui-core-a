a5d1f32425d1187d648e84ed984aeca8db1b56ff
Gilbert D. Cuerbo <gilbert.cuerbo@gmail.com>
2022-03-05 22:28:50 +0800

    add minor changes

--- src/pages/account/components/accountProfileEdit.js
+++ src/pages/account/components/accountProfileEdit.js
@@ -1,11 +1,42 @@
 import React, { useState } from 'react'
 import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
 
 const AccountProfileEdit = (props) => {
 
     return (
         <Box>
-            Account Profile Edit
+            A<Grid container spacing={2}>
+                <Grid item xs={12}>
+                    Account Profile Edit
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                        required
+                        id="outlined-required"
+                        label="Required"
+                        defaultValue="Hello World" />
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                        required
+                        id="outlined-required"
+                        label="Required"
+                        defaultValue="Hello World" />
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                        required
+                        id="outlined-required"
+                        label="Required"
+                        defaultValue="Hello World" />
+                </Grid>
+                <Grid item xs={12}>
+                    <Button variant="contained">Primary</Button>
+                </Grid>
+            </Grid>
         </Box>
     )
 }
--- src/pages/account/components/accountSettingsEdit.js
+++ src/pages/account/components/accountSettingsEdit.js
@@ -1,11 +1,42 @@
 import React, { useState } from 'react'
 import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
 
 const AccountSettingsEdit = (props) => {
 
     return (
         <Box>
-            Account Settings Edit
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    Account Settings Edit
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                        required
+                        id="outlined-required"
+                        label="Required"
+                        defaultValue="Hello World" />
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                        required
+                        id="outlined-required"
+                        label="Required"
+                        defaultValue="Hello World" />
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                        required
+                        id="outlined-required"
+                        label="Required"
+                        defaultValue="Hello World" />
+                </Grid>
+                <Grid item xs={12}>
+                    <Button variant="contained">Primary</Button>
+                </Grid>
+            </Grid>
         </Box>
     )
 }
