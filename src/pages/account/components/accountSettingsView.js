b561aa2f15aaa63c2ee31488d1d6c4e0118efe10
Gilbert D. Cuerbo <gilbert.cuerbo@gmail.com>
2022-03-10 23:02:17 +0800

    add initial layout to some views

--- src/pages/account/components/accountSettingsView.js
+++ src/pages/account/components/accountSettingsView.js
@@ -1,11 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
+import AccountContext from '../../../common/context/accountContext'
+
 
 const AccountSettingsView = (props) => {
 
     return (
         <Box>
             Account Settings View
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={6}></Grid>
+            </Grid>
         </Box>
     )
 }
