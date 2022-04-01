a730cd3a8fad635e1d8c72ee676e1d2c16624b9d
Gilbert D. Cuerbo <gilbert.cuerbo@gmail.com>
2022-04-01 22:16:30 +0800

    initial subpages layout

---
+++ src/common/layouts/subPagesLayout.js
@@ -0,0 +1,38 @@
+import { useState, useContext } from 'react'
+import { Outlet, Link } from "react-router-dom"
+import Container from '@mui/material/Container'
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+
+import config from '../../config'
+
+const SubPagesLayout = (props) => {
+
+
+    return (
+        <Container maxWidth="lg">
+            <Box>
+                <Grid container spacing={2}>
+                    {/* <Grid item xs={12} style={{textAlign: 'center'}}>
+                        <Typography variant="h4" gutterBottom component="div">
+                            { config.appName }
+                        </Typography>
+                    </Grid> */}
+                    <Grid item xs={12} style={{textAlign: 'center'}}>
+                        <Box>
+                            { props.children? props.children: <Outlet /> }
+                        </Box>
+                    </Grid>
+                    {/* <Grid item xs={12} style={{textAlign: 'center'}}>
+                        <Typography variant="caption" display="block" gutterBottom>
+                            Copyrights 2021
+                        </Typography>
+                    </Grid> */}
+                </Grid>
+            </Box>
+        </Container>
+    )
+}
+
+export default SubPagesLayout
