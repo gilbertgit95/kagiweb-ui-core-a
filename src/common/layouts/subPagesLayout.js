d59e1adcdd90b7b70f9bf012080d54a51ed58ae8
Gilbert D. Cuerbo <gilbert.cuerbo@gmail.com>
2022-04-04 23:25:50 +0800

    add sample menu for inital design of subpageslayout

--- src/common/layouts/subPagesLayout.js
+++ src/common/layouts/subPagesLayout.js
@@ -3,6 +3,13 @@ import { Outlet, Link } from "react-router-dom"
 import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
+import Paper from '@mui/material/Paper'
+import Divider from '@mui/material/Divider'
+import MenuList from '@mui/material/MenuList'
+import MenuItem from '@mui/material/MenuItem'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+import Check from '@mui/icons-material/Check'
 import Typography from '@mui/material/Typography'
 
 import config from '../../config'
@@ -19,13 +26,43 @@ const SubPagesLayout = (props) => {
                             { config.appName }
                         </Typography>
                     </Grid> */}
-                    <Grid item xs={3} style={{textAlign: 'center'}}>
+                    <Grid
+                        item xs={12} md={4} lg={3}
+                        sx={{flexGrow: 1, display: {xs: 'none', md: 'flex'}}}
+                        style={{textAlign: 'right'}}>
                         <Box>
-                            first column
+                            <MenuList dense>
+                                <MenuItem>
+                                    <ListItemText inset>Single</ListItemText>
+                                </MenuItem>
+                                <MenuItem>
+                                    <ListItemText inset>1.15</ListItemText>
+                                </MenuItem>
+                                <MenuItem>
+                                    <ListItemText inset>Double</ListItemText>
+                                </MenuItem>
+                                <MenuItem>
+                                    <ListItemIcon>
+                                        <Check />
+                                    </ListItemIcon>
+                                    Custom: 1.2
+                                </MenuItem>
+                                <Divider />
+                                <MenuItem>
+                                    <ListItemText>Add space before paragraph</ListItemText>
+                                </MenuItem>
+                                <MenuItem>
+                                    <ListItemText>Add space after paragraph</ListItemText>
+                                </MenuItem>
+                                <Divider />
+                                <MenuItem>
+                                    <ListItemText>Custom spacing...</ListItemText>
+                                </MenuItem>
+                            </MenuList>
                             {/* { props.children? props.children: <Outlet /> } */}
                         </Box>
                     </Grid>
-                    <Grid item xs={8} style={{textAlign: 'center'}}>
+                    <Grid item xs={12} md={8} lg={9}  style={{textAlign: 'center'}}>
                         <Box>
                             { props.children? props.children: null }
                         </Box>
