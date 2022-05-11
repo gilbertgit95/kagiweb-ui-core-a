6aafd3923a2a7332850aa5caa5ca3357a495b834
Gilbert D. Cuerbo <gilbert.cuerbo@gmail.com>
2022-05-11 11:04:36 +0800

    update acc cred view

--- src/pages/account/components/accountCredentialsView.js
+++ src/pages/account/components/accountCredentialsView.js
@@ -1,44 +1,88 @@
 // import React, { useState, useEffect, useContext } from 'react'
-import Container from '@mui/material/Container'
+// import Container from '@mui/material/Container'
 // import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
+import Divider from '@mui/material/Divider'
 // import TextField from '@mui/material/TextField'
-import Button from '@mui/material/Button'
+// import Button from '@mui/material/Button'
 // import AccountContext from '../../../common/contexts/accountContext'
 // import VerticalSteps from '../../../common/navs/verticalSteps'
-import LoadingButton from '../../../common/buttons/loadingButton'
+// import LoadingButton from '../../../common/buttons/loadingButton'
 
 
 const AccountView = (props) => {
 
     return (
-        <Grid container spacing={2}>
-            <Grid item xs={12} sm={12} md={4}>
-                <Typography
-                    color='primary'
-                    variant='h6'>
-                    Base Credentials
-                </Typography>
-            </Grid>
+        <>
+            <Grid
+                container>
+                <Grid item xs={12} sm={12} md={4}>
+                    <Typography
+                        color='primary'
+                        variant='h6'
+                        style={{
+                            marginBottom: 15
+                        }}>
+                        Base Credentials
+                    </Typography>
 
-            <Grid item xs={12} sm={12} md={4}>
-                <Typography
-                    color='primary'
-                    variant='h6'>
-                    Emails Adresses
-                </Typography>
-            </Grid>
+                    <Typography
+                        variant='body1'>
+                        Username: username
+                    </Typography>
+                    <Typography
+                        variant='body1'>
+                        Type: Super Admin
+                    </Typography>
+                </Grid>
 
-            <Grid item xs={12} sm={12} md={4}>
-                <Typography
-                    color='primary'
-                    variant='h6'>
-                    Phone Numbers
-                </Typography>
-            </Grid>
+                <Grid item xs={12} sm={12} md={4}>
+                    <Typography
+                        color='primary'
+                        variant='h6'
+                        style={{
+                            marginBottom: 15
+                        }}>
+                        Emails Adresses
+                    </Typography>
 
-        </Grid>
+                    <Typography
+                        variant='body1'>
+                        Primary: test101@gmail.com
+                    </Typography>
+                    <Typography
+                        variant='body1'>
+                        Secondary: --
+                    </Typography>
+                </Grid>
+
+                <Grid item xs={12} sm={12} md={4}>
+                    <Typography
+                        color='primary'
+                        variant='h6'
+                        style={{
+                            marginBottom: 15
+                        }}>
+                        Phone Numbers
+                    </Typography>
+
+                    <Typography
+                        variant='body1'>
+                        Primary: +639273854600
+                    </Typography>
+                    <Typography
+                        variant='body1'>
+                        Secondary: --
+                    </Typography>
+                </Grid>
+            </Grid>
+            <Divider
+                style={{
+                    marginTop: 20,
+                    marginBottom: 15
+                }} />
+        </>
     )
 }
 
