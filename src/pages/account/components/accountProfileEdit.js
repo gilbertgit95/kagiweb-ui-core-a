2f8fe14680908e16e30559b6221e041e647814e0
Gilbert D. Cuerbo <gilbert.cuerbo@gmail.com>
2022-05-12 08:35:23 +0800

    update acc profile edit

--- src/pages/account/components/accountProfileEdit.js
+++ src/pages/account/components/accountProfileEdit.js
@@ -1,44 +1,209 @@
-// import React, { useState } from 'react'
+import React, { useEffect, useContext } from 'react'
+
 import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+
+import AccountContext from '../../../common/contexts/accountContext'
+import VerticalSteps from '../../../common/navs/verticalStepsNav'
+import RadioList from '../../../common/inputs/radioList'
+import utils from '../../../common/utilities'
 
 const AccountProfileEdit = (props) => {
+    const accountCtx = useContext(AccountContext)
+
+    const steps = [
+        {
+            icon: null,
+            title: 'Basic Profile',
+            component: (
+                <Grid container spacing={2}>
+                    <Grid item xs={12} sm={12} md={6}
+                        style={styles.grid}>
+                        <TextField
+                            style={styles.fields}
+                            fullWidth
+                            label="First Name" />
+                        <TextField
+                            style={styles.fields}
+                            fullWidth
+                            label="Middle Name" />
+                        <TextField
+                            style={styles.fields}
+                            fullWidth
+                            label="Last Name" />
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={6}
+                        style={styles.grid}>
+                        <TextField
+                            style={styles.fields}
+                            fullWidth
+                            label="Nick Name" />
+                        <Typography
+                            color='primary'
+                            style={{...styles.fields, ...{ marginTop: 15 }}}
+                            variant='body1'>
+                            Gender
+                        </Typography>
+                        <Box style={{ padding: 30, paddingTop: 15 }}>
+                            <RadioList
+                                radioListName='Gender'
+                                onChange={(e) => {}}
+                                selected={'male'}
+                                colSize={{xs: 6}}
+                                list={[
+                                    { disabled: false, key: 'male', label: 'Male'},
+                                    { disabled: false, key: 'female', label: 'Female'}
+                                ]} />
+                        </Box>
+                    </Grid>
+                </Grid>
+            ),
+            action: async () => {
+                console.log('Basic Profile')
+                await utils.waitFor(1)
+                return true
+            }
+        },
+        {
+            icon: null,
+            title: 'Advance Profile',
+            component: (
+                <Grid container spacing={2}>
+                    <Grid item xs={12} sm={12} md={6}
+                        style={styles.grid}>
+                        <TextField
+                            style={styles.fields}
+                            fullWidth
+                            label="Birth Date" />
+                        <TextField
+                            style={styles.fields}
+                            fullWidth
+                            label="Birth Place" />
+                        <TextField
+                            style={styles.fields}
+                            fullWidth
+                            label="Nationality" />
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={6}
+                        style={styles.grid}>
+                        <TextField
+                            style={styles.fields}
+                            rows={2}
+                            multiline
+                            fullWidth
+                            label="Home Address" />
+                        <TextField
+                            style={styles.fields}
+                            rows={3}
+                            multiline
+                            fullWidth
+                            label="Bio" />
+                    </Grid>
+                </Grid>
+            ),
+            action: async () => {
+                console.log('Advance Profile')
+                await utils.waitFor(1)
+                return true
+            }
+        },
+        {
+            icon: null,
+            title: 'Work Related Profile',
+            component: (
+                <Grid container spacing={2}>
+                    <Grid item xs={12} sm={12} md={6}
+                        style={styles.grid}>
+                        <TextField
+                            style={styles.fields}
+                            fullWidth
+                            label="Job Title" />
+                        <TextField
+                            style={styles.fields}
+                            fullWidth
+                            label="Company Name" />
+                        <TextField
+                            style={styles.fields}
+                            rows={2}
+                            multiline
+                            fullWidth
+                            label="Company Description" />
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={6}
+                        style={styles.grid}>
+                        <TextField
+                            style={styles.fields}
+                            fullWidth
+                            label="Contact Email" />
+                        <TextField
+                            style={styles.fields}
+                            fullWidth
+                            label="Contact Number" />
+                        <TextField
+                            style={styles.fields}
+                            rows={2}
+                            multiline
+                            fullWidth
+                            label="Work Address" />
+                    </Grid>
+                </Grid>
+            ),
+            action: async () => {
+                console.log('Work Related Profile')
+                await utils.waitFor(1)
+                return true
+            }
+        }
+    ]
+
+    let finalView = {
+        component: (
+            <>
+                <Typography>
+                    All steps completed. Please see the change details before
+                    saving.
+                </Typography>
+            </>
+        ),
+        action: async () => {
+            console.log('finish button')
+
+            await utils.waitFor(1)
+
+            return true
+        }
+    }
+
+    useEffect(() => {
+        console.log('data in account edit: ', accountCtx.accountContext)
+
+    },[accountCtx.accountContext])
 
     return (
-        <Box>
-            <Grid container spacing={2}>
-                <Grid item xs={12} md={6}>
-                    Account Profile Edit
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <TextField
-                        required
-                        id="outlined-required"
-                        label="Required"
-                        defaultValue="Hello World" />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField
-                        required
-                        id="outlined-required"
-                        label="Required"
-                        defaultValue="Hello World" />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField
-                        required
-                        id="outlined-required"
-                        label="Required"
-                        defaultValue="Hello World" />
-                </Grid>
-                <Grid item xs={12}>
-                    <Button variant="contained">Primary</Button>
-                </Grid>
+        <Grid container spacing={2}>
+            <Grid item xs={12}>
+                <VerticalSteps
+                    nextBtnLabel={ 'Save and Next' }
+                    finishBtnlabel={ 'Save and Finish' }
+                    finalBtnLabel={ 'Save Changes' }
+                    disableLabelClick={ false }
+                    finalView={ finalView }
+                    views={ steps } />
             </Grid>
-        </Box>
+        </Grid>
     )
+}
+
+const styles = {
+    grid: {
+        paddingTop: 0
+    },
+    fields: {
+        marginTop: 10
+    }
 }
 
 export default AccountProfileEdit
