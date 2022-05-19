b1f345004c3f47581a0e3e16f1289605f9cdd4cd
Gilbert D. Cuerbo <gilbert.cuerbo@gmail.com>
2022-05-19 10:27:06 +0800

    intial import table/bulk import

---
+++ src/common/inputs/importTable.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react'
+
+const ImportTable = (props) => {
+    const tableData = useState({
+        header: [],
+        data: []
+    })
+
+    useEffect(() => {
+        // mounted
+        (() => {
+
+        })()
+
+        // unmounted
+        return () => {
+
+        }
+    }, [])
+
+    return (
+        <>
+        </>
+    )
+}
+
+export default ImportTable
