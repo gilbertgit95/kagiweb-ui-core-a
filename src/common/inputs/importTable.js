9ea71d89c17a22a0dc07430b63d6dfb7ec3873fe
Gilbert D. Cuerbo <gilbert.cuerbo@gmail.com>
2022-05-19 10:27:45 +0800

    add references/ notes for import table

--- src/common/inputs/importTable.js
+++ src/common/inputs/importTable.js
@@ -1,5 +1,19 @@
 import { useState, useEffect } from 'react'
 
+// - import excel through copy paste
+// - import excel through upload
+// - option to create table
+
+// - create table from clipboard
+// * edit and evaluation mode
+// * create editable table
+// * editable column
+// * editable row
+// * dragable column
+// * dragable row
+// * add/remove column
+// * add/remove row
+
 const ImportTable = (props) => {
     const tableData = useState({
         header: [],
