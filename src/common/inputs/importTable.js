076548142a40c93a24d23dc8f82b2f93a957d7e1
Gilbert D. Cuerbo <gilbert.cuerbo@gmail.com>
2022-05-23 07:03:58 +0800

    initial horizontale stepper

--- src/common/inputs/importTable.js
+++ src/common/inputs/importTable.js
@@ -1,11 +1,6 @@
 import { useState, useEffect } from 'react'
 
-import Box from '@mui/material/Box'
-import Stepper from '@mui/material/Stepper'
-import Step from '@mui/material/Step'
-import StepLabel from '@mui/material/StepLabel'
-import Button from '@mui/material/Button'
-import Typography from '@mui/material/Typography'
+import HorizontalStepsNav from '../navs/horizontalStepsNav'
 
 // - import excel through copy paste
 // - import excel through upload
@@ -26,106 +21,10 @@ const ImportTable = (props) => {
         header: ['Endpoint', 'Name', 'Type', 'Category', 'Subcategory'],
         data: []
     })
-    const steps = ['Import Data', 'Modify Data', 'Evaluate and Finish']
-
-    const [activeStep, setActiveStep] = useState(0)
-    const [skipped, setSkipped] = useState(new Set())
-
-    const isStepOptional = (step) => {
-        return step === 1
-    }
-
-    const isStepSkipped = (step) => {
-        return skipped.has(step)
-    }
-
-    const handleNext = () => {
-        let newSkipped = skipped
-        if (isStepSkipped(activeStep)) {
-        newSkipped = new Set(newSkipped.values())
-        newSkipped.delete(activeStep)
-        }
-
-        setActiveStep((prevActiveStep) => prevActiveStep + 1)
-        setSkipped(newSkipped)
-    }
-
-    const handleBack = () => {
-        setActiveStep((prevActiveStep) => prevActiveStep - 1)
-    }
-
-    const handleSkip = () => {
-        if (!isStepOptional(activeStep)) {
-        // You probably want to guard against something like this,
-        // it should never occur unless someone's actively trying to break something.
-        throw new Error("You can't skip a step that isn't optional.")
-        }
-
-        setActiveStep((prevActiveStep) => prevActiveStep + 1)
-        setSkipped((prevSkipped) => {
-        const newSkipped = new Set(prevSkipped.values())
-        newSkipped.add(activeStep)
-        return newSkipped
-        })
-    }
-
-    const handleReset = () => {
-        setActiveStep(0)
-    }
+    const steps = ['Import Data', 'Modify Data', 'Evaluate and Save']
 
     return (
-        <Box sx={{ width: '100%' }}>
-            <Stepper activeStep={activeStep}>
-                {steps.map((label, index) => {
-                const stepProps = {}
-                const labelProps = {}
-
-                if (isStepSkipped(index)) {
-                    stepProps.completed = false
-                }
-                return (
-                    <Step key={label} {...stepProps}>
-                    <StepLabel {...labelProps}>{label}</StepLabel>
-                    </Step>
-                )
-                })}
-            </Stepper>
-            {activeStep === steps.length ? (
-                <>
-                <Typography sx={{ mt: 2, mb: 1 }}>
-                    All steps completed - you&aposre finished
-                </Typography>
-                <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-                    <Box sx={{ flex: '1 1 auto' }} />
-                    <Button onClick={handleReset}>Reset</Button>
-                </Box>
-                </>
-            ) : (
-                <>
-                <Typography sx={{ mt: 2, mb: 1 }}>Step {activeStep + 1}</Typography>
-                <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-                    <Button
-                    color="inherit"
-                    disabled={activeStep === 0}
-                    onClick={handleBack}
-                    sx={{ mr: 1 }}
-                    >
-                    Back
-                    </Button>
-                    <Box sx={{ flex: '1 1 auto' }} />
-                    {isStepOptional(activeStep) && (
-                    <Button color="inherit" onClick={handleSkip} sx={{ mr: 1 }}>
-                        Skip
-                    </Button>
-                    )}
-
-                    <Button onClick={handleNext}>
-                    {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-                    </Button>
-                </Box>
-                </>
-            )}
-        </Box>
+        <HorizontalStepsNav />
     )
 }
 
