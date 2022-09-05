0a391ae6262246423643c8bc3eeb37b6ef211417
Gilbert D. Cuerbo <gilbert.cuerbo@gmail.com>
2022-09-06 06:40:02 +0800

    add timer to auto redirect from not found page to signin

--- src/pages/notFound/index.js
+++ src/pages/notFound/index.js
@@ -1,10 +1,34 @@
+import {useState, useEffect} from 'react'
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import Link from '@mui/material/Link'
+import CircularProgress from '@mui/material/CircularProgress'
+
 import InitialLoadinglayout from '../../common/layouts/initalLoadingLayout'
 import config from '../../config'
+import utils from '../../common/utilities'
 
 const NotFound = (props) => {
+    let initialCount = 5
+    let [countdown, setCountdown] = useState(initialCount)
+
+    useEffect(() => {
+        
+        const init = async () => {
+            for (let i = 0; i <= initialCount; i++) {
+                setCountdown(countdown - i)
+                await utils.waitFor(1)
+
+                if ((countdown - i) <= 0) {
+                    window.location.replace(`/${ config.rootRoute }/auth/`);
+                    break
+                }
+            }
+        }
+
+        init()
+
+    }, [initialCount])
 
     return (
         <InitialLoadinglayout>
@@ -23,6 +47,16 @@ const NotFound = (props) => {
                         variant='subtitle1'>
                         This page does not exist, or you are currently logged out.
                         Try signing in to access the page.
+                    </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <CircularProgress style={{width: 15, height: 15}} component='span' />
+                    <Typography
+                        style={{marginLeft: 10}}
+                        color='primary'
+                        component='span'
+                        variant='subtitle1'>
+                        Redirecting to signin page in { countdown }...
                     </Typography>
                 </Grid>
                 <Grid item xs={12}>
