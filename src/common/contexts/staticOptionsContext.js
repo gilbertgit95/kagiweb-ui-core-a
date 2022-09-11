3b059a60ea52707c0232349f9a652b426870ff80
Gilbert Cuerbo <gilbert.cuerbo@gmail.com>
2022-09-11 22:29:05 +0800

    update for inital static options loading

--- src/common/contexts/staticOptionsContext.js
+++ src/common/contexts/staticOptionsContext.js
@@ -1,4 +1,5 @@
-import { useState, createContext } from 'react'
+import { useState, useEffect, createContext } from 'react'
+import Rest from '../datasource/rest/'
 
 const StaticOptionsContext = createContext({
     staticOptionsContext: {
@@ -12,6 +13,20 @@ export const useStaticOptionsContext = () => {
     const [staticOptionsContext, setStaticOptionsContext] = useState({
         countries: {isLoading: false, list: []}
     })
+
+    useEffect(() => {
+        const init = async () => {
+          let countries = []
+
+          try {
+            countries = await Rest.staticOpons.getCountries()
+          } catch(err) {
+
+          }
+        }
+        // fetch
+        init()
+    }, [])
 
     return {staticOptionsContext, setStaticOptionsContext}
 }
--- src/common/datasource/rest/index.js
+++ src/common/datasource/rest/index.js
@@ -1,9 +1,11 @@
 import auth from './auth'
 import account from './account'
 import loggedAccount from './loggedAccount'
+import staticOptions from './staticOptions'
 
 export default {
     auth,
     account,
-    loggedAccount
+    loggedAccount,
+    staticOptions
 }
