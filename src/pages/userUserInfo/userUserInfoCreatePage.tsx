4b44e7039daca64d2b7d4621d321c25bc6ec8842
Gilbert Cuerbo <gilbert.cuerbo@gmail.com>
2024-03-05 09:01:06 +0800

    updates for user info read only view

--- src/pages/userUserInfo/userUserInfoCreatePage.tsx
+++ src/pages/userUserInfo/userUserInfoCreatePage.tsx
@@ -1,1 +1,91 @@
-export default {}
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { Container, Button, Box, Divider } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+import PrimaryHeader from '../../components/headers/primaryHeader';
+import { IUser } from '../../types/user';
+import ResponseStatus, { TResponseStatus } from '../../components/infoOrWarnings/responseStatus';
+import UserService from '../user/userService';
+import UserUserInfosReadOnlyView from './userUserInfosReadOnlyView';
+
+const UserUserInfosPage = () => {
+    const { userId } = useParams()
+    const navigate = useNavigate()
+    const [user, setUser] = useState<IUser | undefined>()
+    const [infoAndErrors, setInfoAndErrors] = useState<TResponseStatus>({
+        errorMessages: [],
+        infoMessages: []
+    })
+
+    useEffect(() => {
+        const init = async () => {
+            if (userId) {
+                try {
+                    const userResp = await UserService.getUser(userId)
+                    setUser(userResp.data)
+                } catch (err:any) {
+                    console.log(err)
+                    setInfoAndErrors({
+                        ...{infoMessages: []},
+                        ...{errorMessages: [err?.response?.data?.message || '']}
+                    })
+                }
+            }
+        }
+        console.log('initiate UserInfo features page')
+        init()
+    }, [userId])
+
+    return (
+        <Container style={{paddingTop: 20}}>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <PrimaryHeader title={'User User Infos View'} subtitle={ user?.username } />
+                    <Divider />
+                </Grid>
+                <Grid item xs={6}>
+                    <Button
+                        variant="text"
+                        startIcon={<ArrowBackIosNewIcon />}
+                        onClick={() => navigate(-1)}>
+                        Back
+                    </Button>
+                </Grid>
+                <Grid item xs={6} style={{alignContent: 'right'}}>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'flex-end',
+                        }}>
+                        <Button
+                            variant="text"
+                            startIcon={<AddIcon />}
+                            onClick={() => navigate(`/users/create/${ userId }/userInfos`)}>
+                            Edit
+                        </Button>
+                        <Button
+                            variant="text"
+                            color="secondary"
+                            startIcon={<DeleteIcon />}
+                            onClick={() => console.log('onDelete')}>
+                            Create
+                        </Button>
+                    </Box>
+                </Grid>
+
+                <UserUserInfosReadOnlyView user={user} />
+
+                <Grid item xs={12}>
+                    <ResponseStatus {...infoAndErrors} />
+                </Grid>
+            </Grid>
+        </Container>
+    )
+}
+
+export default UserUserInfosPage
