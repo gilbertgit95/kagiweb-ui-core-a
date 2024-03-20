42274580ee40e5262235ed4e85a3d55b2eacb22a
Gilbert Cuerbo <gilbert.cuerbo@gmail.com>
2024-03-20 11:14:13 +0800

    update hompages

--- src/pages/home/privateHomePage.tsx
+++ src/pages/home/privateHomePage.tsx
@@ -1,7 +1,26 @@
-const Home = () => {
+import { Typography, Grid, Button } from '@mui/material'
+// import TrendingFlatIcon from '@mui/icons-material/TrendingFlat';
+
+const PrivateLandingPage = () => {
     return (
-        <div>Private Home Page</div>
+        <Grid
+            container
+            spacing={2}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            sx={{ minHeight: '80vh' }}>
+            <Grid item xs={12}>
+                <Typography color='primary' variant='h5'>Welcome! this your Dashboard</Typography>
+            </Grid>
+            {/* <Grid item xs={12}>
+                <Button
+                    variant='outlined'
+                    onClick={() => window.location.replace('/signin')}
+                    endIcon={<TrendingFlatIcon />}>go to signin page</Button>
+            </Grid> */}
+        </Grid>
     )
 }
 
-export default Home
+export default PrivateLandingPage
--- src/pages/home/publicHomePage.tsx
+++ src/pages/home/publicHomePage.tsx
@@ -1,6 +1,25 @@
+import { Typography, Grid, Button } from '@mui/material'
+import TrendingFlatIcon from '@mui/icons-material/TrendingFlat';
+
 const PublicLandingPage = () => {
     return (
-        <div>public Home page</div>
+        <Grid
+            container
+            spacing={2}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            sx={{ minHeight: '80vh' }}>
+            <Grid item xs={12}>
+                <Typography color='primary' variant='h5'>Welcome! this the home page</Typography>
+            </Grid>
+            <Grid item xs={12}>
+                <Button
+                    variant='outlined'
+                    onClick={() => window.location.replace('/signin')}
+                    endIcon={<TrendingFlatIcon />}>go to signin page</Button>
+            </Grid>
+        </Grid>
     )
 }
 
