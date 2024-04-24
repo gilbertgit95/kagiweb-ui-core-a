10ed5efac71d31435b381183ffb9eb2cc9308e86
Gilbert Cuerbo <gilbert.cuerbo@gmail.com>
2024-04-24 21:18:51 +0800

    init code for app utility

---
+++ src/utils/appUtils.ts
@@ -0,0 +1,11 @@
+
+
+export default {
+    // this will get the store token in localstorage and
+    // set it to the api service, then
+    // fetch init user data, then
+    // set this data to the global state
+    loadSigninUserData() {
+
+    }
+}
