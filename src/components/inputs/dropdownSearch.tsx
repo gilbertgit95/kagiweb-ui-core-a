e6ee4f762270b0d59fc01693d1022a44a0544181
Gilbert Cuerbo <gilbert.cuerbo@gmail.com>
2024-04-27 11:25:24 +0800

    init dropdown select

--- src/components/inputs/dropdownSearch.tsx
+++ src/components/inputs/dropdownSearch.tsx
@@ -1,1 +1,97 @@
-export default {}
+import React from 'react'
+import { Divider, TextField } from '@mui/material';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import SearchIcon from '@mui/icons-material/Search';
+import InputAdornment from '@mui/material/InputAdornment';
+
+interface IOption {
+    key: string,
+    label: string,
+    Icon?: React.FC
+}
+
+interface IProps {
+    selected?: string,
+    options: IOption[],
+    ariaLabel: string,
+    ariaControls: string,
+    placeholder?: string
+}
+
+const DropDownSearch = (props: IProps) => {
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
+
+    const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget)
+    }
+
+    const handleClose = () => {
+        setAnchorEl(null)
+    }
+
+    const handleSelect = (selected:string) => {
+        console.log(selected)
+    }
+
+    return (
+        <>
+            <TextField
+                aria-label={props.ariaLabel}
+                aria-controls={props.ariaControls}
+                aria-haspopup="true"
+                placeholder={props.placeholder || 'Search'}
+                InputProps={{
+                    startAdornment: (
+                    <InputAdornment position="start">
+                        <SearchIcon />
+                    </InputAdornment>
+                    ),
+                }}
+                onClick={handleMenu} />
+            <Menu
+                id={props.ariaControls}
+                // anchorPosition={{
+                //     vertical: 'top',
+                //     horizontal: 'left'
+                // }}
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}>
+                <TextField
+                    sx={{marginRight: '10px', marginLeft: '10px'}}
+                    placeholder={props.placeholder || 'Search'}
+                    InputProps={{
+                        startAdornment: (
+                        <InputAdornment position="start">
+                            <SearchIcon />
+                        </InputAdornment>
+                        ),
+                    }} />
+                {
+                    props.options.map((item, index) => {
+                        return (
+                            <React.Fragment key={index}>
+                                <MenuItem onClick={() => handleSelect(item.key)}>
+                                    {
+                                        item.Icon? (
+                                            <ListItemIcon>
+                                                <item.Icon />
+                                            </ListItemIcon>
+                                        ): null
+                                    }
+                                    <ListItemText>{ item.label }</ListItemText>
+                                </MenuItem>
+                                { index !== props.options?.length - 1? <Divider />: null }
+                            </React.Fragment>
+                        )
+                    })
+                }
+            </Menu>
+        </>
+    )
+}
+
+export default DropDownSearch
--- src/layouts/workspaceDataLayout.tsx
+++ src/layouts/workspaceDataLayout.tsx
@@ -1,19 +1,45 @@
 import React from 'react'
-import { Container, Grid, Divider } from '@mui/material';
-import PrimaryHeader from '../components/headers/primaryHeader';
+import { Container, Grid } from '@mui/material';
+
+import ScatterPlotIcon from '@mui/icons-material/ScatterPlot';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import DropDownSearch from '../components/inputs/dropdownSearch';
 
 interface Props {
     children?: React.ReactNode
 }
 
 const WorkspaceDataLayout = (props:Props) => {
+   
     return (
         <Container style={{paddingTop: 20}}>
             <Grid container spacing={2}>
-                <Grid item xs={12}>
-                    <PrimaryHeader title={'User Roles Update View'} subtitle={ 'test' } />
-                    <Divider />
+                <Grid item xs={6}>
+                    {/* <PrimaryHeader title={'User Roles Update View'} subtitle={ 'test' } />
+                    <Divider /> */}
+                    <DropDownSearch
+                        ariaLabel="Search Select Workspace"
+                        ariaControls='select-workspace-menu'
+                        selected={''}
+                        options={[
+                            {
+                                key: 'opt1',
+                                label: 'Option 1',
+                                Icon: BookmarkIcon
+                            },
+                            {
+                                key: 'opt2',
+                                label: 'Option 2',
+                                Icon: ScatterPlotIcon
+                            },
+                            {
+                                key: 'opt3',
+                                label: 'Option 3',
+                                Icon: ScatterPlotIcon
+                            }
+                        ]} />
                 </Grid>
+                <Grid item xs={6}></Grid>
                 { props.children }
             </Grid>
         </Container>
