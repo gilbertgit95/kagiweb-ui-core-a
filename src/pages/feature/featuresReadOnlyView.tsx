95cef5200a29a89affa09d6859b90f9daf0fe970
Gilbert Cuerbo <gilbert.cuerbo@gmail.com>
2024-04-30 21:08:10 +0800

    init features readOnly view

---
+++ src/pages/feature/featuresReadOnlyView.tsx
@@ -0,0 +1,134 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Container, Button, Box, Divider } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import FeaturedPlayListIcon from '@mui/icons-material/FeaturedPlayList';
+// import VisibilityIcon from '@mui/icons-material/Visibility';
+
+import PrimaryHeader from '../../components/headers/primaryHeader';
+import PrimaryTable, { IColDef } from '../../components/tables/primaryTable';
+import DateChanges, {IChangeDate} from '../../components/dates/dateChanges';
+import ListItems from '../../components/lists/listItems';
+import ShortendDescription from '../../components/texts/shortendDescription';
+import SimpleLink from '../../components/links/simpleLink';
+import { useAppSelector} from '../../stores/appStore';
+import { IFeature } from '../../types/feature';
+
+interface IFeatureRow {
+    _id: string,
+    name: string,
+    value: string,
+    type: string,
+    tags: string[]
+}
+
+const colDef:IColDef[] = [
+    {
+        header: 'Name',
+        field: '',
+        Component: (props:IFeatureRow & IChangeDate) => {
+            return <ShortendDescription value={props.name} />
+        }
+    },
+    {
+        header: 'Value',
+        field: '',
+        Component: (props:IFeatureRow & IChangeDate) => {
+            return <ShortendDescription value={props.value} />
+        }
+    },
+    {
+        header: 'Type',
+        field: 'type',
+        Component: undefined // react Component or undefined
+    },
+    {
+        header: 'Tags',
+        field: '',
+        Component: (props:IFeatureRow & IChangeDate) => {
+            return <ListItems items={props.tags} />
+        }
+    },
+    {
+        header: 'Changed',
+        field: '',
+        Component: (props:IFeatureRow & IChangeDate) => {
+            return <DateChanges {...props} />
+        }
+    },
+    {
+        header: '',
+        field: '',
+        Component: (props:IFeatureRow & IChangeDate) => {
+            return (
+                <SimpleLink
+                    link={`/features/view/${ props._id }`}
+                    text="View Feature" />
+            )
+        }
+    }
+]
+
+const FeaturesReadOnlyView = () => {
+    const navigate = useNavigate()
+    const features:IFeature[] = useAppSelector(state => state.appRefs.features) || []
+    const [data, setData] = useState<(IFeatureRow & IChangeDate)[]>([])
+
+    useEffect(() => {
+        const init = async () => {
+            try {
+                if (features) {
+                    const tarnsformedData:(IFeatureRow & IChangeDate)[] = features.map((item:IFeature & IChangeDate) => {
+                        return {
+                            _id: item._id || '',
+                            name: item.name || '--',
+                            value: item.value || '--',
+                            type: item.type || '--',
+                            tags: item.tags || [],
+                            createdAt: item.createdAt,
+                            updatedAt: item.updatedAt
+                        }
+                    })
+                    setData(tarnsformedData)
+                }
+            } catch (err) {
+            }
+        }
+        console.log('initiate features page')
+        init()
+    }, [features])
+
+
+    return (
+        <Container style={{paddingTop: 20}}>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <PrimaryHeader title={'Feature List View'} />
+                    <Divider />
+                </Grid>
+                <Grid item xs={12}>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'flex-end',
+                        }}>
+                        <Button
+                            variant="text"
+                            startIcon={<FeaturedPlayListIcon />}
+                            onClick={() => navigate('/features/create')}>
+                            Create Feature
+                        </Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={12}>
+                    <PrimaryTable
+                        maxHeight={700}
+                        columnDefs={colDef}
+                        data={data} />
+                </Grid>
+            </Grid>
+        </Container>
+    )
+}
+
+export default FeaturesReadOnlyView
