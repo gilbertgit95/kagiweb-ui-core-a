ad244da89ec7d14ea5651961116c8329ad924884
Gilbert Cuerbo <gilbert.cuerbo@gmail.com>
2024-05-05 10:32:43 +0800

    init tree component

--- src/components/headers/primaryHeader.tsx
+++ src/components/headers/primaryHeader.tsx
@@ -14,14 +14,14 @@ const PrimaryHeader = ({title, subtitle, Icon}:Props) => {
     return (
         <Stack
             direction="row"
-            style={{padding:'10px'}}
+            style={{padding:"10px"}}
             alignItems="center"
             spacing={2}>
             { Icon? <Icon />: <VisibilityIcon /> }
-            <Typography variant='h5'>
+            <Typography variant="h5">
                  { title }
             </Typography>
-            <Typography color="primary" variant='subtitle2'>
+            <Typography color="primary" variant="subtitle2">
                  { subtitle }
             </Typography>
         </Stack>
--- src/components/navs/treeDirectory.tsx
+++ src/components/navs/treeDirectory.tsx
@@ -1,19 +1,84 @@
-import { Typography } from "@mui/material"
+import { useState, useEffect } from 'react'
+import { Box, Button } from '@mui/material'
 
 export interface IDir {
     name: string,
     subDir: IDir[]
 }
 
+export interface IRecursiveDir {
+    name: string,
+    subDir: IDir[],
+    selected?: string[],
+    parents?: string[],
+    onClick?: (items:string[]) => void
+}
+
 interface IProps {
     directories?: IDir[]
 }
 
-const TreeDirectory = (props:IProps) => {
+
+const RecursiveComponent = (props:IRecursiveDir) => {
+    const parents = [...(props.parents? props.parents: []), ...[props.name]]
+    const isCurrentSelected = props.selected?.join('') === parents?.join('')
+
     return (
-        <>
-            <Typography>Tree View</Typography>
-        </>
+        <Box
+            key={parents.join('_')}
+            sx={{
+                paddingLeft: '10px',
+                borderLeft: '1px solid',
+                borderLeftColor: 'primary.main'
+            }}>
+            <Button
+                variant={isCurrentSelected? 'contained': 'text'}
+                onClick={() => {
+                    if (props.onClick) props.onClick(parents)
+                }}>{ props.name }</Button>
+            <Box
+                sx={{
+                    paddingLeft: '20px'
+                }}>
+                {
+                    props.subDir.map(item => RecursiveComponent({
+                        ...item,
+                        ...{
+                            parents,
+                            selected: props.selected,
+                            onClick: props.onClick
+                        }
+                    }))
+                }
+            </Box>
+        </Box>
+    )
+}
+
+const TreeDirectory = (props:IProps) => {
+    const [selected, setSelected] = useState<string[]>(['Root', 'Owner', 'Contact Infos'])
+
+    const onClick = (parents:string[]) => {
+        setSelected(parents)
+    }
+
+    return (
+        <Box
+            sx={{
+                // paddingLeft: '20px',
+                // borderTop: '1px solid black',
+                paddingTop: '10px',
+                paddingBottom: '10px',
+            }}>
+            {
+                RecursiveComponent({
+                    name: 'Root',
+                    subDir: props.directories || [],
+                    selected,
+                    onClick
+                })
+            }
+        </Box>
     )
 }
 
