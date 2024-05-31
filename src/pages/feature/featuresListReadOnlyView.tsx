4d3886c99b8ec7419a6d049198a417ee31ddb390
Gilbert Cuerbo <gilbert.cuerbo@gmail.com>
2024-05-31 17:32:26 +0800

    cleanup

--- src/pages/feature/featuresListReadOnlyView.tsx
+++ src/pages/feature/featuresListReadOnlyView.tsx
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-// import { Container, Button, Box, Divider } from '@mui/material';
 import Grid from '@mui/material/Grid';
-// import FeaturedPlayListIcon from '@mui/icons-material/FeaturedPlayList';
-// import VisibilityIcon from '@mui/icons-material/Visibility';
 
 import PrimaryHeader from '../../components/headers/primaryHeader';
 import PrimaryTable, { IColDef } from '../../components/tables/primaryTable';
