dc4ebe67b36b66a0f6d1b2b175ac935194f2ecde
Gilbert Cuerbo <gilbert.cuerbo@gmail.com>
2024-06-12 23:09:00 +0800

    updates for private routes setting in app components handler

--- src/routes/privateRoutes.tsx
+++ src/routes/privateRoutes.tsx
@@ -1,219 +1,30 @@
+import { useMemo } from 'react';
 import { Grid, Button } from '@mui/material'
 import TrendingFlatIcon from '@mui/icons-material/TrendingFlat';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PrivatePageLayout from '../layouts/privatePageLayout';
 import NotFoundPage from '../components/infoOrWarnings/pageNotFound';
 
-import PrivateHomePage from '../pages/home/privateHomePage';
-import WorkspaceHomePage from '../pages/home/workspacesHomePage';
-
-import FeaturesPage from '../pages/feature/featuresPage';
-import FeaturePage from '../pages/feature/featurePage';
-import FeatureCreatePage from '../pages/feature/featureCreatePage';
-import FeatureEditPage from '../pages/feature/featureEditPage';
-
-import RolesPage from '../pages/role/rolesPage';
-import RolePage from '../pages/role/rolePage';
-import RoleCreatePage from '../pages/role/roleCreatePage';
-import RoleEditPage from '../pages/role/roleEditPage';
-
-import RoleFeaturesPage from '../pages/roleFeature/roleFeaturesPage';
-import RoleFeaturesEditPage from '../pages/roleFeature/roleFeaturesEditPage';
-
-import UsersPage from '../pages/user/usersPage';
-import UserCreatePage from '../pages/user/userCreatePage';
-import UserPage from '../pages/user/userPage';
-import UserEditPage from '../pages/user/userEditPage';
-
-import UserRolesPage from '../pages/userRole/userRolesPage';
-import UserRolesEditPage from '../pages/userRole/userRolesEditPage';
-
-import UserPasswordsPage from '../pages/userPassword/userPasswordsPage';
-import UserPasswordCreatePage from '../pages/userPassword/userPasswordCreatePage';
-
-import UserLimitedTransactionsPage from '../pages/userLimitedTransaction/userLimitedTransactionsPage';
-import UserLimitedTransactionPage from '../pages/userLimitedTransaction/userLimitedTransactionPage';
-import UserLimitedTransactionEditPage from '../pages/userLimitedTransaction/userLimitedTransactionEditPage';
-
-import UserClientDevicesPage from '../pages/userClientDevice/userClientDevicesPage';
-import UserClientDevicePage from '../pages/userClientDevice/userClientDevicePage';
-import UserClientDeviceCreatePage from '../pages/userClientDevice/userClientDeviceCreatePage';
-import UserClientDeviceEditPage from '../pages/userClientDevice/userClientDeviceEditPage';
-
-import UserClientDeviceTokensPage from '../pages/userClientDeviceToken/userClientDeviceTokensPage';
-import UserClientDeviceTokenPage from '../pages/userClientDeviceToken/userClientDeviceTokenPage';
-import UserClientDeviceTokenCreatePage from '../pages/userClientDeviceToken/userClientDeviceTokenCreatePage';
-import UserClientDeviceTokenEditPage from '../pages/userClientDeviceToken/userClientDeviceTokenEditPage';
-
-import UserContactInfosPage from '../pages/userContactInfo/userContactInfosPage';
-import UserContactInfoPage from '../pages/userContactInfo/userContactInfoPage';
-import UserContactInfoEditPage from '../pages/userContactInfo/userContactInfoEditPage';
-import UserContactInfoCreatePage from '../pages/userContactInfo/userContactInfoCreatePage';
-
-import UserUserInfosPage from '../pages/userUserInfo/userUserInfosPage';
-import UserUserInfoPage from '../pages/userUserInfo/userUserInfoPage';
-import UserUserInfoEditPage from '../pages/userUserInfo/userUserInfoEditPage';
-import UserUserInfoCreatePage from '../pages/userUserInfo/userUserInfoCreatePage';
-
-import UserWorkspacesPage from '../pages/userWorkspace/userWorkspacesPage';
-import UserWorkspacePage from '../pages/userWorkspace/userWorkspacePage';
-import UserWorkspaceCreatePage from '../pages/userWorkspace/userWorkspaceCreatePage';
-import UserWorkspaceEditPage from '../pages/userWorkspace/userWorkspaceEditPage';
-
-import UserWorkspaceUserRefsPage from '../pages/userWorkspaceUserRef/userWorkspaceUserRefsPage';
-import UserWorkspaceUserRefPage from '../pages/userWorkspaceUserRef/userWorkspaceUserRefPage';
-import UserWorkspaceUserRefCreatePage from '../pages/userWorkspaceUserRef/userWorkspaceUserRefCreatePage';
-import UserWorkspaceUserRefEditPage from '../pages/userWorkspaceUserRef/userWorkspaceUserRefEditPage';
-
-
-import OwnerPage from '../pages/owner/ownerPage';
-import OwnerEditPage from '../pages/owner/ownerEditPage';
-import OwnerUserInfosPage from '../pages/owner/ownerUserInfosPage';
-import OwnerUserInfoCreatePage from '../pages/owner/ownerUserInfoCreatePage';
-import OwnerUserInfoEditPage from '../pages/owner/ownerUserInfoEditPage';
-import OwnerUserInfoPage from '../pages/owner/ownerUserInfoPage';
-import OwnerContactInfosPage from '../pages/owner/ownerContactInfosPage';
-import OwnerContactInfoCreatePage from '../pages/owner/ownerContactInfoCreatePage';
-import OwnerContactInfoPage from '../pages/owner/ownerContactInfoPage';
-import OwnerContactInfoEditPage from '../pages/owner/ownerContactInfoEditPage';
-import OwnerRolesPage from '../pages/owner/ownerRolesPage';
-import OwnerRolesEditPage from '../pages/owner/ownerRolesEditPage';
-import OwnerLimitedTransactionsPage from '../pages/owner/ownerLimitedTransactionsPage';
-import OwnerLimitedTransactionPage from '../pages/owner/ownerLimitedTransactionPage';
-import OwnerLimitedTransactionEditPage from '../pages/owner/ownerLimitedTransactionEditPage';
-import OwnerPasswordsPage from '../pages/owner/ownerPasswordsPage';
-import OwnerPasswordCreatePage from '../pages/owner/ownerPasswordCreatePage';
-import OwnerClientDevicesPage from '../pages/owner/ownerClientDevicesPage';
-import OwnerClientDevicePage from '../pages/owner/ownerClientDevicePage';
-import OwnerClientDeviceCreatePage from '../pages/owner/ownerClientDeviceCreatePage';
-import OwnerClientDeviceEditPage from '../pages/owner/ownerClientDeviceEditPage';
-import OwnerClientDeviceTokensPage from '../pages/owner/ownerClientDeviceTokensPage';
-import OwnerClientDeviceTokenPage from '../pages/owner/ownerClientDeviceTokenPage';
-import OwnerClientDeviceTokenCreatePage from '../pages/owner/ownerClientDeviceTokenCreatePage';
-import OwnerClientDeviceTokenEditPage from '../pages/owner/ownerClientDeviceTokenEditPage';
-import OwnerWorkspacesPage from '../pages/owner/ownerWorkspacesPage';
-import OwnerWorkspacePage from '../pages/owner/ownerWorkspacePage';
-import OwnerWorkspaceCreatePage from '../pages/owner/ownerWorkspaceCreatePage';
-import OwnerWorkspaceEditPage from '../pages/owner/ownerWorkspaceEditPage';
-import OwnerWorkspaceUserRefsPage from '../pages/owner/ownerWorkspaceUserRefsPage';
-import OwnerWorkspaceUserRefPage from '../pages/owner/ownerWorkspaceUserRefPage';
-import OwnerWorkspaceUserRefCreatePage from '../pages/owner/ownerWorkspaceUserRefCreatePage';
-import OwnerWorkspaceUserRefEditPage from '../pages/owner/ownerWorkspaceUserRefEditPage';
+import appComponentsHandler from '../utils/appComponentsHandler';
 
 const PrivateRoutes = () => {
+    const privateRoutes = useMemo(() => {
+        return appComponentsHandler.routes.privateRoutes
+    }, [])
+
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<PrivatePageLayout />}>
-                    <Route index element={<PrivateHomePage />} />
-
-                    {/* feature pages */}
-                    <Route path="features" element={<FeaturesPage />} />
-                    <Route path="features/create" element={<FeatureCreatePage />} />
-                    <Route path="features/view/:featureId" element={<FeaturePage />} />
-                    <Route path="features/edit/:featureId" element={<FeatureEditPage />} />
-
-                    {/* role pages */}
-                    <Route path="roles" element={<RolesPage />} />
-                    <Route path="roles/create" element={<RoleCreatePage />} />
-                    <Route path="roles/view/:roleId" element={<RolePage />} />
-                    <Route path="roles/edit/:roleId" element={<RoleEditPage />} />
-                    {/* role feature pages */}
-                    <Route path="roles/view/:roleId/features" element={<RoleFeaturesPage />} />
-                    <Route path="roles/edit/:roleId/features" element={<RoleFeaturesEditPage />} />
-
-                    {/* user pages */}
-                    <Route path="users" element={<UsersPage />} />
-                    <Route path="users/create" element={<UserCreatePage />} />
-                    <Route path="users/view/:userId" element={<UserPage />} />
-                    <Route path="users/edit/:userId" element={<UserEditPage />} />
-                    {/* user roles */}
-                    <Route path="users/view/:userId/roles" element={<UserRolesPage />} />
-                    <Route path="users/edit/:userId/roles" element={<UserRolesEditPage />} />
-                    {/* user passwords */}
-                    <Route path="users/view/:userId/passwords" element={<UserPasswordsPage />} />
-                    <Route path="users/create/:userId/passwords" element={<UserPasswordCreatePage />} />
-                    {/* user limited transactions */}
-                    <Route path="users/view/:userId/limitedTransactions" element={<UserLimitedTransactionsPage />} />
-                    <Route path="users/view/:userId/limitedTransactions/:limitedTransactionId" element={<UserLimitedTransactionPage />} />
-                    <Route path="users/edit/:userId/limitedTransactions/:limitedTransactionId" element={<UserLimitedTransactionEditPage />} />
-                    {/* user client devices */}
-                    <Route path="users/view/:userId/clientDevices" element={<UserClientDevicesPage />} />
-                    <Route path="users/create/:userId/clientDevices" element={<UserClientDeviceCreatePage />} />
-                    <Route path="users/view/:userId/clientDevices/:clientDeviceId" element={<UserClientDevicePage />} />
-                    <Route path="users/edit/:userId/clientDevices/:clientDeviceId" element={<UserClientDeviceEditPage />} />
-                    {/* user client device tokens */}
-                    <Route path="users/view/:userId/clientDevices/:clientDeviceId/clientDeviceTokens" element={<UserClientDeviceTokensPage />} />
-                    <Route path="users/create/:userId/clientDevices/:clientDeviceId/clientDeviceTokens" element={<UserClientDeviceTokenCreatePage />} />
-                    <Route path="users/view/:userId/clientDevices/:clientDeviceId/clientDeviceTokens/:clientDeviceTokenId" element={<UserClientDeviceTokenPage />} />
-                    <Route path="users/edit/:userId/clientDevices/:clientDeviceId/clientDeviceTokens/:clientDeviceTokenId" element={<UserClientDeviceTokenEditPage />} />
-                    {/* user contact infos */}
-                    <Route path="users/view/:userId/contactInfos" element={<UserContactInfosPage />} />
-                    <Route path="users/view/:userId/contactInfos/:contactInfoId" element={<UserContactInfoPage />} />
-                    <Route path="users/edit/:userId/contactInfos/:contactInfoId" element={<UserContactInfoEditPage />} />
-                    <Route path="users/create/:userId/contactInfos" element={<UserContactInfoCreatePage />} />
-                    {/* user user infos */}
-                    <Route path="users/view/:userId/userInfos" element={<UserUserInfosPage />} />
-                    <Route path="users/view/:userId/userInfos/:userInfoId" element={<UserUserInfoPage />} />
-                    <Route path="users/edit/:userId/userInfos/:userInfoId" element={<UserUserInfoEditPage />} />
-                    <Route path="users/create/:userId/userInfos" element={<UserUserInfoCreatePage />} />
-                    {/* user workspaces */}
-                    <Route path="users/view/:userId/workspaces" element={<UserWorkspacesPage />} />
-                    <Route path="users/create/:userId/workspaces" element={<UserWorkspaceCreatePage />} />
-                    <Route path="users/view/:userId/workspaces/:workspaceId" element={<UserWorkspacePage />} />
-                    <Route path="users/edit/:userId/workspaces/:workspaceId" element={<UserWorkspaceEditPage />} />
-                    {/* user workspace userRefs */}
-                    <Route path="users/view/:userId/workspaces/:workspaceId/userRefs" element={<UserWorkspaceUserRefsPage />} />
-                    <Route path="users/create/:userId/workspaces/:workspaceId/userRefs" element={<UserWorkspaceUserRefCreatePage />} />
-                    <Route path="users/view/:userId/workspaces/:workspaceId/userRefs/:userRefId" element={<UserWorkspaceUserRefPage />} />
-                    <Route path="users/edit/:userId/workspaces/:workspaceId/userRefs/:userRefId" element={<UserWorkspaceUserRefEditPage />} />
-
-                    {/* owner pages */}
-                    <Route path="owner/view" element={<OwnerPage />} />
-                    <Route path="owner/edit" element={<OwnerEditPage />} />
-                    {/* owner user info */}
-                    <Route path="owner/view/userInfos" element={<OwnerUserInfosPage />} />
-                    <Route path="owner/create/userInfos" element={<OwnerUserInfoCreatePage />} />
-                    <Route path="owner/view/userInfos/:userInfoId" element={<OwnerUserInfoPage />} />
-                    <Route path="owner/edit/userInfos/:userInfoId" element={<OwnerUserInfoEditPage />} />
-                    {/* owner contact info */}
-                    <Route path="owner/view/contactInfos" element={<OwnerContactInfosPage />} />
-                    <Route path="owner/create/contactInfos" element={<OwnerContactInfoCreatePage />} />
-                    <Route path="owner/view/contactInfos/:contactInfoId" element={<OwnerContactInfoPage />} />
-                    <Route path="owner/edit/contactInfos/:contactInfoId" element={<OwnerContactInfoEditPage />} />
-                    {/* owner roles */}
-                    <Route path="owner/view/roles" element={<OwnerRolesPage />} />
-                    <Route path="owner/edit/roles" element={<OwnerRolesEditPage />} />
-                    {/* owner limited transaction */}
-                    <Route path="owner/view/limitedTransactions" element={<OwnerLimitedTransactionsPage />} />
-                    <Route path="owner/view/limitedTransactions/:limitedTransactionId" element={<OwnerLimitedTransactionPage />} />
-                    <Route path="owner/edit/limitedTransactions/:limitedTransactionId" element={<OwnerLimitedTransactionEditPage />} />
-                    {/* owner password */}
-                    <Route path="owner/view/passwords" element={<OwnerPasswordsPage />} />
-                    <Route path="owner/create/passwords" element={<OwnerPasswordCreatePage />} />
-                    {/* owner client devices */}
-                    <Route path="owner/view/clientDevices" element={<OwnerClientDevicesPage />} />
-                    <Route path="owner/create/clientDevices" element={<OwnerClientDeviceCreatePage />} />
-                    <Route path="owner/view/clientDevices/:clientDeviceId" element={<OwnerClientDevicePage />} />
-                    <Route path="owner/edit/clientDevices/:clientDeviceId" element={<OwnerClientDeviceEditPage />} />
-                    {/* owner client device tokens */}
-                    <Route path="owner/view/clientDevices/:clientDeviceId/clientDeviceTokens" element={<OwnerClientDeviceTokensPage />} />
-                    <Route path="owner/create/clientDevices/:clientDeviceId/clientDeviceTokens" element={<OwnerClientDeviceTokenCreatePage />} />
-                    <Route path="owner/view/clientDevices/:clientDeviceId/clientDeviceTokens/:clientDeviceTokenId" element={<OwnerClientDeviceTokenPage />} />
-                    <Route path="owner/edit/clientDevices/:clientDeviceId/clientDeviceTokens/:clientDeviceTokenId" element={<OwnerClientDeviceTokenEditPage />} />
-                    {/* owner workspaces */}
-                    <Route path="owner/view/workspaces" element={<OwnerWorkspacesPage />} />
-                    <Route path="owner/create/workspaces" element={<OwnerWorkspaceCreatePage />} />
-                    <Route path="owner/view/workspaces/:workspaceId" element={<OwnerWorkspacePage />} />
-                    <Route path="owner/edit/workspaces/:workspaceId" element={<OwnerWorkspaceEditPage />} />
-                    {/* owner workspace userRefs */}
-                    <Route path="owner/view/workspaces/:workspaceId/userRefs" element={<OwnerWorkspaceUserRefsPage />} />
-                    <Route path="owner/create/workspaces/:workspaceId/userRefs" element={<OwnerWorkspaceUserRefCreatePage />} />
-                    <Route path="owner/view/workspaces/:workspaceId/userRefs/:userRefId" element={<OwnerWorkspaceUserRefPage />} />
-                    <Route path="owner/edit/workspaces/:workspaceId/userRefs/:userRefId" element={<OwnerWorkspaceUserRefEditPage />} />
-
-                    {/* workspace data */}
-                    <Route path="workspaces/view/:workspaceId" element={<WorkspaceHomePage />} />
+                    {
+                        privateRoutes.map((route, routeIndex)  => {
+                            return route.url === 'index'? (
+                                <Route key={routeIndex} index element={<route.page />} />
+                            ): (
+                                <Route key={routeIndex} path={ route.url } element={<route.page />} />
+                            )
+                        })
+                    }
 
                     {/* for none existing page */}
                     <Route
--- src/utils/appComponentsHandler.ts
+++ src/utils/appComponentsHandler.ts
@@ -59,6 +59,6 @@ class AppComponentsHandler {
 
 }
 
-const appComponentsHandler = new AppComponentsHandler
+const appComponentsHandler = new AppComponentsHandler()
 
 export default appComponentsHandler
