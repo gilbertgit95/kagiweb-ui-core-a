cdef2d4fb8a796badebfd5f55564a00460a12dc9
gilbert cuerbo <gilbert.cuerbo@gmail.com>
2024-11-23 08:51:19 +0800

    update feature type

--- src/types/feature.ts
+++ src/types/feature.ts
@@ -1,8 +1,8 @@
 export type TFeatureScope = 'app' | 'account' | 'workspace'
-export type TFeatureType = 'api-route' | 'ui-route' | 'ui-main-drawer' | 'ui-account-drawer' | 'workspace-access'
+export type TFeatureType = 'api-route' | 'ui-route' | 'ui-main-drawer' | 'ui-account-drawer'
 
 export const featureScopes:TFeatureScope[] = ['app', 'account', 'workspace']
-export const featureTypes:TFeatureType[] = ['api-route', 'ui-route', 'ui-main-drawer', 'ui-account-drawer', 'workspace-access']
+export const featureTypes:TFeatureType[] = ['api-route', 'ui-route', 'ui-main-drawer', 'ui-account-drawer']
 
 // create interfaces
 export interface IFeature {
